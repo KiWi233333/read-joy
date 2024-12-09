7a68366d0ba0bc2b7b39485848c567f4f3b31745
Kiwi2333 <1329634286@qq.com>
2024-12-09 11:41:00 +0800

    Update: 添加数据库文件

--- database/ryjiaoyubookmanagesystem.sql
+++ database/ryjiaoyubookmanagesystem.sql
@@ -1,106 +1,134 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : mysql8
-Source Server Version : 80022
-Source Host           : localhost:3308
-Source Database       : ryjiaoyubookmanagesystem
+ Source Server         : 我的电脑
+ Source Server Type    : MySQL
+ Source Server Version : 80039 (8.0.39)
+ Source Host           : localhost:3306
+ Source Schema         : ryjiaoyubookmanagesystem
 
-Target Server Type    : MYSQL
-Target Server Version : 80022
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 80039 (8.0.39)
+ File Encoding         : 65001
 
-Date: 2024-12-04 18:39:13
+ Date: 07/12/2024 16:47:01
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for admin
 -- ----------------------------
 DROP TABLE IF EXISTS `admin`;
-CREATE TABLE `admin` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `admin`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `username` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '管理员用户名',
+  `password` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '管理员密码',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '管理员表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for book
 -- ----------------------------
 DROP TABLE IF EXISTS `book`;
-CREATE TABLE `book` (
-  `bookId` int NOT NULL,
-  `isbn` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `title` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `categoryId` int NOT NULL,
-  `author` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `introduction` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `price` double(10,2) NOT NULL,
-  `publisher` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '出版社',
-  `coverImageUrl` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `publishionDate` datetime NOT NULL,
-  PRIMARY KEY (`bookId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `book`  (
+  `bookId` int NOT NULL COMMENT '书籍ID',
+  `isbn` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '国际标准书号',
+  `title` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '书名',
+  `categoryId` int NOT NULL COMMENT '分类ID',
+  `author` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '作者',
+  `introduction` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '书籍介绍',
+  `price` double(10, 2) NOT NULL COMMENT '价格',
+  `publisher` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '出版社',
+  `coverImageUrl` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '封面图片URL',
+  `publishionDate` datetime NOT NULL COMMENT '出版日期',
+  PRIMARY KEY (`bookId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '书籍表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of book
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for category
 -- ----------------------------
 DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category` (
-  `categoryId` int NOT NULL AUTO_INCREMENT,
-  `categoryName` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`categoryId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `category`  (
+  `categoryId` int NOT NULL AUTO_INCREMENT COMMENT '分类ID',
+  `categoryName` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '分类名称',
+  PRIMARY KEY (`categoryId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '分类表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for comment
 -- ----------------------------
 DROP TABLE IF EXISTS `comment`;
-CREATE TABLE `comment` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `bookId` int NOT NULL,
-  `commentator` int NOT NULL,
-  `commentBody` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '评论的内容',
+CREATE TABLE `comment`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '评论ID',
+  `bookId` int NOT NULL COMMENT '书籍ID',
+  `commentator` int NOT NULL COMMENT '评论者ID',
+  `commentBody` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '评论内容',
   `commentStatus` tinyint(1) NOT NULL COMMENT '评论状态，控制是否暂停评论',
-  `isDeleted` tinyint(1) NOT NULL,
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `isDeleted` tinyint(1) NOT NULL COMMENT '是否删除',
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '评论表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for resource
 -- ----------------------------
 DROP TABLE IF EXISTS `resource`;
-CREATE TABLE `resource` (
-  `resourceId` int NOT NULL,
-  `type` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '类型名，视频，教学大纲，课件等',
-  `bookId` int NOT NULL COMMENT ' 关联的图书的id',
-  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `size` int NOT NULL COMMENT '  资源的标题',
-  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT ' 资源保存的地址',
-  `submitter` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '资源提交者',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `isDeleted` tinyint(1) NOT NULL,
-  `downloadCount` int NOT NULL COMMENT ' 下载数',
+CREATE TABLE `resource`  (
+  `resourceId` int NOT NULL COMMENT '资源ID',
+  `type` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '资源类型，视频，教学大纲，课件等',
+  `bookId` int NOT NULL COMMENT '关联的图书ID',
+  `title` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '资源标题',
+  `size` int NOT NULL COMMENT '资源大小',
+  `url` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '资源保存地址',
+  `submitter` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '资源提交者',
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `isDeleted` tinyint(1) NOT NULL COMMENT '是否删除',
+  `downloadCount` int NOT NULL COMMENT '下载数',
   `likeCount` int NOT NULL COMMENT '点赞数',
-  PRIMARY KEY (`resourceId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`resourceId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '资源表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of resource
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `loginName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `loginPassword` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `trueName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `telephone` varchar(11) NOT NULL,
-  `imgUrl` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `isChecked` tinyint(1) NOT NULL,
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `userType` tinyint(1) NOT NULL COMMENT ' 用户类型',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `user`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `loginName` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '登录名',
+  `loginPassword` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '登录密码',
+  `trueName` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '真实姓名',
+  `telephone` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '电话号码',
+  `imgUrl` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '用户头像URL',
+  `isChecked` tinyint(1) NOT NULL COMMENT '是否审核通过',
+  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `userType` tinyint(1) NOT NULL COMMENT '用户类型',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
