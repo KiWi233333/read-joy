8190fc50da0a599bcb3f0bc7cbad84b3ade31c1c
Kiwi2333 <1329634286@qq.com>
2025-02-24 01:18:15 +0800

    Fix: 兼容mysql - 8.4

--- read-joy-api/src/test/java/com/readjoy/readjoyapi/UtilsTests.java
+++ read-joy-api/src/test/java/com/readjoy/readjoyapi/UtilsTests.java
@@ -2,7 +2,6 @@
 package com.readjoy.readjoyapi;
 
 import com.readjoy.readjoyapi.common.utils.LocalFileUtil;
-import com.readjoy.readjoyapi.common.utils.XFAuthUtil;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -47,12 +46,4 @@ class UtilsTests {
 ////        log.info("size: " + size);
 ////        log.info("size: " + LocalFileUtil.formatSize(size));
 //    }
-
-    @Resource
-    private XFAuthUtil xfAuthUtil;
-
-    @Test
-    void testLocalFiles() throws IOException {
-        System.out.println(xfAuthUtil.getAuthorizationUrl());
-    }
 }
